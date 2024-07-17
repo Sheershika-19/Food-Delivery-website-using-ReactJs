44e53fbbe08fabbb536a16066ca6e673ff09cb44
Sheershika reddy thotli <122619319+Sheershika-19@users.noreply.github.com>
2024-07-17 11:38:43 +0530

    Add files via upload

---
+++ src/assets/assets.js
@@ -0,0 +1,350 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.jpeg'
+import header_img from './header_img.jpeg'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+import food_1 from './food_1.jpg'
+import food_2 from './food_2.jpg'
+import food_3 from './food_3.png'
+import food_4 from './food_4.jpg'
+import food_5 from './food_5.jpg'
+import food_6 from './food_6.jpg'
+import food_7 from './food_7.jpg'
+import food_8 from './food_8.jpg'
+import food_9 from './food_9.jpg'
+import food_10 from './food_10.jpg'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.jpg'
+import food_14 from './food_14.jpg'
+import food_15 from './food_15.jpg'
+import food_16 from './food_16.jpg'
+import food_17 from './food_17.jpg'
+import food_18 from './food_18.jpg'
+import food_19 from './food_19.jpg'
+import food_20 from './food_20.jpg'
+import food_21 from './food_21.jpg'
+import food_22 from './food_22.jpg'
+import food_23 from './food_23.jpg'
+import food_24 from './food_24.jpg'
+import food_25 from './food_25.jpg'
+import food_26 from './food_26.jpg'
+import food_27 from './food_27.jpg'
+import food_28 from './food_28.jpg'
+import food_29 from './food_29.jpg'
+import food_30 from './food_30.jpg'
+import food_31 from './food_31.jpg'
+import food_32 from './food_32.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Salad",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Rolls",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Deserts",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Sandwich",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Cake",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Pure Veg",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Pasta",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Noodles",
+        menu_image: menu_8
+    }]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Greek salad",
+        image: food_1,
+        price: 12,
+        description: "Greek salad with tomatoes, cucumbers, olives, and feta.",
+        category: "Salad"
+    },
+    {
+        _id: "2",
+        name: "Veg salad",
+        image: food_2,
+        price: 18,
+        description: "Veg salad with lettuce, carrots, peppers, and cucumbers.",
+        category: "Salad"
+    }, {
+        _id: "3",
+        name: "Clover Salad",
+        image: food_3,
+        price: 16,
+        description: "Clover salad with clover, onions, tomatoes, and dressing.",
+        category: "Salad"
+    }, {
+        _id: "4",
+        name: "Chicken Salad",
+        image: food_4,
+        price: 24,
+        description: "Chicken salad with chicken, celery, mayonnaise, and herbs.",
+        category: "Salad"
+    }, {
+        _id: "5",
+        name: "Lasagna Rolls",
+        image: food_5,
+        price: 14,
+        description: "Lasagna rolls with pasta, ricotta, spinach, and marinara.",
+        category: "Rolls"
+    }, {
+        _id: "6",
+        name: "Peri Peri Rolls",
+        image: food_6,
+        price: 12,
+        description: "Peri Peri rolls with peri peri chicken, lettuce, mayo, and sauce.",
+        category: "Rolls"
+    }, {
+        _id: "7",
+        name: "Chicken Rolls",
+        image: food_7,
+        price: 20,
+        description: "Chicken rolls with shredded chicken, vegetables, mayo, and seasoning.",
+        category: "Rolls"
+    }, {
+        _id: "8",
+        name: "Veg Rolls",
+        image: food_8,
+        price: 15,
+        description: "Veg rolls with mixed vegetables, tofu, sauce, and seasoning.",
+        category: "Rolls"
+    }, {
+        _id: "9",
+        name: "Ripple Ice Cream",
+        image: food_9,
+        price: 14,
+        description: "Ripple ice cream with chocolate swirls, vanilla base, and toppings.",
+        category: "Deserts"
+    }, {
+        _id: "10",
+        name: "Fruit Ice Cream",
+        image: food_10,
+        price: 22,
+        description: "Fruit ice cream with mixed fruits, creamy base, and sweetness.",
+        category: "Deserts"
+    }, {
+        _id: "11",
+        name: "Jar Ice Cream",
+        image: food_11,
+        price: 10,
+        description: "Jar ice cream with layers of flavors, toppings, and indulgence.",
+        category: "Deserts"
+    }, {
+        _id: "12",
+        name: "Vanilla Ice Cream",
+        image: food_12,
+        price: 12,
+        description: "Vanilla ice cream with creamy texture and classic flavor.",
+        category: "Deserts"
+    },
+    {
+        _id: "13",
+        name: "Chicken Sandwich",
+        image: food_13,
+        price: 12,
+        description: "Chicken sandwich with grilled chicken, lettuce, tomato, and mayo.",
+        category: "Sandwich"
+    },
+    {
+        _id: "14",
+        name: "Vegan Sandwich",
+        image: food_14,
+        price: 18,
+        description: "Vegan sandwich with hummus, avocado, lettuce, and tomato.",
+        category: "Sandwich"
+    }, {
+        _id: "15",
+        name: "Grilled Sandwich",
+        image: food_15,
+        price: 16,
+        description: "Grilled sandwich with cheese, ham, and vegetables.",
+        category: "Sandwich"
+    }, {
+        _id: "16",
+        name: "Bread Sandwich",
+        image: food_16,
+        price: 24,
+        description: "Bread sandwich with slices of bread and butter",
+        category: "Sandwich"
+    }, {
+        _id: "17",
+        name: "Cup Cake",
+        image: food_17,
+        price: 14,
+        description: "Chocolate cupcake with rich cocoa flavor and frosting.",
+        category: "Cake"
+    }, {
+        _id: "18",
+        name: "Vegan Cake",
+        image: food_18,
+        price: 12,
+        description: "Vegan cake made without animal products",
+        category: "Cake"
+    }, {
+        _id: "19",
+        name: "Butterscotch Cake",
+        image: food_19,
+        price: 20,
+        description: "Butterscotch cake with caramelized sugar and creamy frosting.",
+        category: "Cake"
+    }, {
+        _id: "20",
+        name: "Rainbow Cake",
+        image: food_20,
+        price: 15,
+        description: "Rainbow cake with colorful layers and buttercream frosting.",
+        category: "Cake"
+    }, {
+        _id: "21",
+        name: "Garlic Mushroom ",
+        image: food_21,
+        price: 14,
+        description: "Garlic mushrooms sautéed in butter with herbs",
+        category: "Pure Veg"
+    }, {
+        _id: "22",
+        name: "Fried Cauliflower",
+        image: food_22,
+        price: 22,
+        description: "Fried cauliflower with crispy batter coating",
+        category: "Pure Veg"
+    }, {
+        _id: "23",
+        name: "Mix Veg Pulao",
+        image: food_23,
+        price: 10,
+        description: "Mixed veg pulao with assorted vegetables and fragrant rice",
+        category: "Pure Veg"
+    }, {
+        _id: "24",
+        name: "Rice Zucchini",
+        image: food_24,
+        price: 12,
+        description: "Rice with zucchini, a simple and nutritious dish",
+        category: "Pure Veg"
+    },
+    {
+        _id: "25",
+        name: "Cheese Pasta",
+        image: food_25,
+        price: 12,
+        description: "Cheese pasta with creamy sauce and Parmesan",
+        category: "Pasta"
+    },
+    {
+        _id: "26",
+        name: "Tomato Pasta",
+        image: food_26,
+        price: 18,
+        description: "Tomato pasta with tangy tomato sauce",
+        category: "Pasta"
+    }, {
+        _id: "27",
+        name: "Creamy Pasta",
+        image: food_27,
+        price: 16,
+        description: "Creamy pasta with Alfredo sauce",
+        category: "Pasta"
+    }, {
+        _id: "28",
+        name: "Chicken Pasta",
+        image: food_28,
+        price: 24,
+        description: "Chicken pasta with tender chicken pieces",
+        category: "Pasta"
+    }, {
+        _id: "29",
+        name: "Butter Noodles",
+        image: food_29,
+        price: 14,
+        description: "Butter noodles with rich buttery sauce",
+        category: "Noodles"
+    }, {
+        _id: "30",
+        name: "Veg Noodles",
+        image: food_30,
+        price: 12,
+        description: "Veg noodles with stir-fried vegetables",
+        category: "Noodles"
+    }, {
+        _id: "31",
+        name: "Somen Noodles",
+        image: food_31,
+        price: 20,
+        description: "Somen noodles, a type of thin Japanese noodle",
+        category: "Noodles"
+    }, {
+        _id: "32",
+        name: "Cooked Noodles",
+        image: food_32,
+        price: 15,
+        description: "Cooked noodles ready for seasoning",
+        category: "Noodles"
+    }
+]
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food</h2>
+            <p>Enjoy delicious food that will make your taste buds happy and leave you wanting more. Our fresh ingredients and tasty recipes are sure to satisfy everyone.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
